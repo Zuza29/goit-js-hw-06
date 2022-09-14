ab3b37fcf29da7dc323a47b49016115b7cf2748d
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2022-09-14 15:32:58 +0200

    Fix

--- js/task-03.js
+++ js/task-03.js
@@ -13,17 +13,20 @@ const images = [
   },
 ];
 
-let listOfImages = document.querySelectorAll('.gallery');
-if (listOfImages && listOfImages[0].classList) {
-  listOfImages[0].classList.add('container');
+let listOfImages = document.querySelector('.gallery');
+console.log(listOfImages);
+
+if (listOfImages && listOfImages.classList) {
+  listOfImages.classList.add('container');
 }
 
 images.forEach((imageItem) => {
   const listItem = document.createElement("li");
   const image = document.createElement("img");
   image.src = imageItem.url;
+  image.alt = imageItem.alt;
   listItem.appendChild(image);
-  listOfImages[0].insertAdjacentHTML('beforeend', `<li style="padding: 50px; list-style-type: none; display: inline;" ><img height="200" width="300" src="${imageItem.url}" style="border: solid blue 5px"/></li>`)
+  listOfImages.insertAdjacentHTML('beforeend', `<li style="padding: 50px; list-style-type: none; display: inline;" ><img height="200" width="300" src="${imageItem.url}" alt="${imageItem.alt} "style="border: solid blue 5px"/></li>`)
 
 })
 
--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,6 @@
 const inputElement = document.getElementById("name-input");
 const outputElement = document.getElementById("name-output");
 
-console.log(inputElement, outputElement);
-
 const addInput = (event) => {
 
     if (event.target.value) {
